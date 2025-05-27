2782115a739b01edf814ad7915f7b53107db49f6
Muhammad Farooq <fm6182218@gmail.com>
2025-05-28 00:01:18 +0500

    JavaScript pract

---
+++ app.js
@@ -0,0 +1,70 @@
+// Q1:
+
+// let a = 3;
+// let b = 4;
+
+// let c = a + b;
+// console.log( `The Sum is : ${c}`);
+
+// ******* //
+
+// Q2:
+
+// function num(input) {
+//     if (input > 0) {
+//         return `Positive`
+//     } else if (input < 0) {
+//         return `Nagetive`
+//     } else  {
+//         return `Zero`
+//     }
+// }
+// console.log(num(0));
+
+// ******* //
+
+// Q3:
+
+// let x = 6;
+// let y = 5;
+
+// let c = x * y;
+
+// console.log(`The Multiply is : ${c}`);
+
+// ******* // 
+
+// Q4: 
+
+
+// function check(num) {
+//     if (num % 2 === 0) {
+//         return "Even";
+//     }else {
+//         return "Odd";
+//     }
+// }
+// console.log(check(-2));
+
+// ******* //
+
+// Q5:
+
+// let a = 10;
+// let b = 25;
+
+function twoNumber(a, b) {
+    if(a>b){
+        return a
+    }else if(b>a){
+        return b
+    }else{
+        return "Both are equal "
+    }
+}
+console.log(twoNumber(100, 23));
+
+
+
+// ******* // 
+
